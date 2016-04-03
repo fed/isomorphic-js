700f7b01557237cfb92fbeddb584a51895e4c7dd
Federico Knüssel <fknussel@gmail.com>
2016-04-03 23:36:27 +1000

    Add intro to isomorphic apps

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,45 +1,42 @@
 /* global module:false */
 module.exports = function(grunt) {
-	var port = grunt.option('port') || 6789;
-	var base = grunt.option('base') || '.';
+  var port = grunt.option('port') || 6789;
+  var base = grunt.option('base') || '.';
 
-	// Project configuration
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
+  // Project configuration
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-		connect: {
-			server: {
-				options: {
-					port: port,
-					base: base,
-					livereload: true,
-					open: true
-				}
-			}
-		},
+    connect: {
+      server: {
+        options: {
+          port: port,
+          base: base,
+          livereload: true,
+          open: true
+        }
+      }
+    },
 
-		watch: {
-			options: {
-				livereload: true
-			},
-			gruntfile: {
-				files: [ 'Gruntfile.js' ]
-			},
-			html: {
-				files: [ 'index.html']
-			}
-		}
+    watch: {
+      options: {
+        livereload: true
+      },
+      gruntfile: {
+        files: ['Gruntfile.js']
+      },
+      html: {
+        files: ['index.html']
+      }
+    }
 
-	});
+  });
 
-	// Dependencies
-	grunt.loadNpmTasks( 'grunt-contrib-watch' );
-	grunt.loadNpmTasks( 'grunt-contrib-connect' );
+  // Load tasks
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
 
-	// Serve presentation locally
-	grunt.registerTask( 'serve', [ 'connect', 'watch' ] );
-
-	// Default task
-	grunt.registerTask( 'default', [ 'serve' ] );
-
+  // Register custom tasks
+  grunt.registerTask('serve', ['connect', 'watch']);
+  grunt.registerTask('default', ['serve']);
 };
