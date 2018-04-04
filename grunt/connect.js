47c5c9280e90f5b30e6716c4210aeb2efed3247c
Federico Knüssel <fknussel@gmail.com>
2018-04-04 17:35:38 -0300

    Copy reveal.js node_module to build folder

--- grunt/connect.js
+++ grunt/connect.js
@@ -2,7 +2,7 @@ module.exports = {
   server: {
     options: {
       port: 6789,
-      base: ['build', '.'],
+      base: ['.', 'build', 'node_modules'],
       livereload: true,
       open: true
     }
--- grunt/copy.js
+++ grunt/copy.js
@@ -3,8 +3,14 @@ module.exports = {
     expand: true,
     cwd: 'src/assets',
     src: '**',
-    dest: 'build/',
+    dest: 'build',
     flatten: true,
     filter: 'isFile'
+  },
+  vendor: {
+    expand: true,
+    cwd: 'node_modules/reveal.js',
+    src: '**',
+    dest: 'build/reveal.js'
   }
 };
